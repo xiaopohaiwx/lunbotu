e028fb05ec0e62c872d3f585f986d85b7bb31d68
xiaopohaiwx <1309232650@qq.com>
2018-06-05 22:50:22 +0800

    轮播图未加点击按钮

---
+++ 轮播图.js
@@ -0,0 +1,82 @@
+var imgs = document.getElementById("imgs");
+var lis = document.getElementsByTagName("li");
+var t1 = null; //定义一个定时器变量
+var init = 0; // 定义一个变量表示第几张图片
+//第一种定时方法：
+var t1 = setInterval(function(){    //设置定时器
+    init++;
+    init %= 6;  //用于循环到第六张时，使init恢复为0，重新进行循环
+    imgs.style.left = init * -300 + 'px'; //控制imgs左移几张图片的距离
+    for(var i = 0; i < lis.length; i++)
+    {
+        lis[i].className = "";  //不给任何li加on类名，用于清除on的样式
+    }
+    lis[init].className = "on"; //
+}, 1000)
+
+//第二种定时方法：
+// var t1 = setTimeout(function(){
+//     init++;
+//     init %= 6;
+//     imgs.style.left = init * -300 + 'px';
+//     for(var i = 0; i < 6; i++)
+//     {
+//         lis[i].className = "";
+//     }
+//     lis[init].className = "on";
+//     if(init < 6)
+//     {
+//         setTimeout(arguments.callee,1000);
+//     } 
+// }, 1000);
+
+function start()
+{
+    t1 = setInterval(function(){
+        init++;
+        init %= 6;
+        imgs.style.left = init * -300 + 'px';
+        for(var j = 0; j < 6; j++)
+        {
+            lis[j].className = "";
+        }
+        lis[init].className = "on";        
+    }, 1000);
+}
+
+
+imgs.onmousemove = function()
+{
+    clearInterval(t1);
+}
+imgs.onmouseout = function()
+{
+    start();
+}
+
+for(var k = 0; k < lis.length; k++)
+{
+    lis[k].index = k;
+    lis[k].onmouseover = function()
+    {
+        for(var m = 0; m < lis.length; m++)
+        {
+            lis[m].className = "";
+        }
+        this.className = "on";
+        clearInterval(t1);
+        init = this.index;
+        imgs.style.left = init * -300 + 'px';
+    }
+}
+lis.onmouseout = function()
+{
+    start();
+}
+
+
+
+
+
+
+
